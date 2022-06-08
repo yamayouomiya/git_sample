bab0205d2c1994cab61d54b9746c243e8f30b59f
yamayouomiya <jzuoteng764@gmail.com>
2022-06-08 20:11:54 +0900

    add sumFunction sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+int sum(int,int);
+
 int main(){
     int a=5;
     int b=3;
-    std::cout<<a+b<<std::endl;
+    std::cout <<sum(a,b)<<std::endl;
     return 0;
+}
+
+int sum(int a,int b)
+{
+    return a+b;
 }
